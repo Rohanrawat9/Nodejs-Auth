87d1c83019b41a60d4f97f08bc23642a6ac5530c
Rohanrawat9 <126374291+Rohanrawat9@users.noreply.github.com>
2023-05-26 22:41:22 +0530

    Add files via upload

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://Rohan-Rawat:W6tz6kSVuyJy1Bt3@cluster0.waglp2n.mongodb.net/?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const customFlash = require('./config/flash');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+//------------ Passport Configuration ------------//
+require('./config/passport')(passport);
+
+//------------ DB Configuration ------------//
+const db = require('./config/key').MongoURI;
+
+//------------ Mongo Connection ------------//
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Successfully connected to MongoDB"))
+    .catch(err => console.log(err));
+
+//------------ EJS Configuration ------------//
+app.use(expressLayouts);
+app.use("/assets", express.static('./assets'));
+app.set('view engine', 'ejs');
+
+//------------ Bodyparser Configuration ------------//
+app.use(express.urlencoded({ extended: false }))
+
+//------------ Express session Configuration ------------//
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+//------------ Passport Middlewares ------------//
+app.use(passport.initialize());
+app.use(passport.session());
+
+//------------ Connecting flash ------------//
+app.use(flash());
+//------------ Global variables ------------//
+app.use(customFlash.setFlash);
+//------------ Routes ------------//
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+
+app.listen(PORT,function (err) {
+    if(err){
+        console.log(err);
+    }else {
+        console.log(`Server Started At Port ${PORT}`);
+    }
+});
